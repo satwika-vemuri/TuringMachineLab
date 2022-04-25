e0bd22c36ca6db12e3c9252efe9f745610567e3a
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2022-04-25 11:55:12 -0400

    Update TurningMachineRunner.java

--- TurningMachineRunner.java
+++ TurningMachineRunner.java
@@ -1,16 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TurningMachineRunner {
-  
+
     public static void main(String[] args) {
-        // Read in input
 
-        // this.tape = new Tape(args);
-        // this.stateMachine = new StateMachine(args);
+        Tape tape = new Tape("00100", 0);
+
+        State S0 = new State("S0", new ArrayList<Transition>(Arrays.asList(new Transition("0", "S1", "0", 1))), false);
+        State S1 = new State("S1", new ArrayList<Transition>(Arrays.asList(new Transition("0", "S2", "0", 1))), false); 
+        State S2 = new State("S2", new ArrayList<Transition>(Arrays.asList((new Transition("0", "A", "0", 1)), (new Transition(null, null, null, 0)))), true); 
+        State A = new State("A", new ArrayList<Transition>(Arrays.asList()), false);
+
+        ArrayList<State> states = new ArrayList<State>(
+            Arrays.asList(S0, S1, S2, A));
+        StateMachine stateMachine = new StateMachine(new ArrayList<State>());
+
+
+
+
+
     }
 
-    private Tape runMachine(Tape t, StateMachine sm) {
-        
+    public String runMachine(Tape tape, ArrayList symbols) {
+
 
     }
+
 
 
 }
