ebebf2c2b1a6d3ed9af6fd8efa94a98e5d3def7f
Satwika Vemuri <satwika.vemuri@gmail.com>
2022-04-28 13:14:27 -0400

    5 states working

--- TurningMachineRunner.java
+++ TurningMachineRunner.java
@@ -7,20 +7,21 @@ public class TurningMachineRunner {
 
     public static void main(String[] args) {
 
-        String input = "#000000#";
+        String input = "#000#";
         Tape tape = new Tape(input, input.length()-2);
 
         Transition[] startTransitions = {(new Transition("1", "0", "carry", 0)), (new Transition("0", "1", "add", 0))};    
         Transition[] carryTransitions = {(new Transition("1", "0", "carry", 0)), (new Transition("#", "#", "done", 0)), (new Transition("0", "1", "add", 0))};
         Transition[] doneTransitions = {};    
         Transition[] addTransitions = {(new Transition("1", "1", "add", 0)), (new Transition("0", "0", "add", 0)), (new Transition("#", "#", "leftHash", 0))};
-        Transition[] leftHashTransitions = {(new Transition("1", "1", "leftHash", 0)), (new Transition("0", "1", "toLeftHash", 0))};
-        Transition[] toLeftHashTransitions = {(new Transition("1", "1", "toLeftHash", 1)), (new Transition("0", "1", "L1", 0))};
+        Transition[] leftHashTransitions = {(new Transition("1", "1", "leftHash", 0)), (new Transition("0", "1", "L1", 0))};
         Transition[] L1Transitions = {(new Transition("0", "1", "L2", 0))};
         Transition[] L2Transitions = {(new Transition("0", "1", "L3", 0))};
         Transition[] L3Transitions =  {(new Transition("0", "1", "L4", 0))};;
-        Transition[] L4Transitions = {(new Transition("0", "1", "toLeftHash", 0))};
-        Transition[] toRightHashTransitions = {(new Transition("1", "1", "toRightHash", 1)), (new Transition("0", "0", "toRightHash", 1)), (new Transition("#", "#", "moveRight", 1))};    
+        Transition[] L4Transitions = {(new Transition("0", "1", "toLeftHash", 1))};
+        Transition[] toLeftHashTransitions = {(new Transition("1", "1", "toLeftHash", 1)), (new Transition("#", "#", "toRightHash", 1))};
+    
+        Transition[] toRightHashTransitions = {(new Transition("1", "1", "toRightHash", 1)), (new Transition("0", "0", "toRightHash", 1)), (new Transition("#", "#", "moveRight", 1))};
         Transition[] moveRightTransitions = {(new Transition("1", "1", "moveRight", 1 )), (new Transition("0", "1", "R1", 1))};
         Transition[] R1Transitions =  {(new Transition("0", "1", "R2", 1))};
         Transition[] R2Transitions =  {(new Transition("0", "1", "R3", 1))};
@@ -43,7 +44,7 @@ public class TurningMachineRunner {
         State R1 = new State ("R1", R1Transitions, false, 11); 
         State R2 = new State ("R2", R2Transitions, false, 12); 
         State R3 = new State ("R3", R3Transitions, false, 13); 
-        State R4 = new State ("R1", R4Transitions, false, 14); 
+        State R4 = new State ("R4", R4Transitions, false, 14); 
         State backToRightHash = new State("backToRightHash", backToRightHashTransitions, false, 15);
         State done = new State("done", doneTransitions, true, 16);
 
@@ -53,13 +54,15 @@ public class TurningMachineRunner {
         StateMachine stateMachine = new StateMachine(states);
         int ones = runMachine(tape, stateMachine);
         System.out.printf("Final one count: %d\n", ones);
-        System.out.printf("Final score: %.5f", (ones / (17 + 3 + input.length())));
+        System.out.printf("Final score: %.5f", (Float.valueOf(ones) / (17 + 3 + input.length())));
     }
 
     public static int runMachine(Tape tape, StateMachine sm) {
         while (!sm.getCurrentState().isTerminal) {
             String input =  tape.read();
+            System.out.println(input);
             Transition transition = sm.move(input);
+            System.out.println(sm.getCurrentState().name);
             tape.write(transition.getWrite());
             if (transition.getDirection() == 0) {
                 tape.left();
